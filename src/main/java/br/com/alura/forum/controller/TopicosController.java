29a440771100612257ebc0c9e25ecd2355753188
George Paiva <george.paiva@capgemini.com>
2021-03-24 10:24:29 -0300

    Add Spring actuator

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -44,11 +44,10 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 
 	@GetMapping
-	@Cacheable(value= "listaDeTopicos")
-	public Page<TopicoDto> lista(@RequestParam(required=false) String nomeCurso, 
-			@PageableDefault(sort="id", direction = Direction.ASC, page = 0, size = 10) Pageable paginacao) {
-		
-		
+	@Cacheable(value = "listaDeTopicos")
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
+			@PageableDefault(sort = "id", direction = Direction.ASC, page = 0, size = 10) Pageable paginacao) {
+
 		if (nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
 			return TopicoDto.converter(topicos);
@@ -61,7 +60,7 @@ public class TopicosController {
 
 	@PostMapping
 	@Transactional
-	@CacheEvict(value= "listaDeTopicos", allEntries = true)
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
@@ -83,7 +82,7 @@ public class TopicosController {
 
 	@PutMapping("/{id}")
 	@Transactional
-	@CacheEvict(value= "listaDeTopicos", allEntries = true)
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
 		Optional<Topico> optional = topicoRepository.findById(id);
 		if (optional.isPresent()) {
@@ -97,7 +96,7 @@ public class TopicosController {
 
 	@DeleteMapping("/{id}")
 	@Transactional
-	@CacheEvict(value= "listaDeTopicos", allEntries = true)
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<?> remover(@PathVariable Long id) {
 		Optional<Topico> optional = topicoRepository.findById(id);
 		if (optional.isPresent()) {
